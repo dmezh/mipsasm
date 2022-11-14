63047ce30439429d068c30a711c25b3cd2e05a28
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-11-14 17:53:05 -0500

    Parse input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+use clap::Parser as ClapParser;
+use pest::Parser as PestParser;
+
+#[derive(pest_derive::Parser)]
+#[grammar = "mipsasm.pest"]
+struct MipsParser;
+
+#[derive(clap::Parser)]
+struct Args {
+    input: String
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let input = std::fs::read_to_string(args.input)?;
+
+    let pairs = MipsParser::parse(Rule::program, &input)?;
+
+    println!("{:#?}", pairs);
+
+    Ok(())
 }
