b9d9f3b3748a98f53e2b40da6c8a6ef7a2b2869f
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-11-14 18:39:34 -0500

    Ready to emit hex

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 use clap::Parser as ClapParser;
-use pest::Parser as PestParser;
+use pest::{iterators::Pairs, Parser as PestParser};
 
 #[derive(pest_derive::Parser)]
 #[grammar = "mipsasm.pest"]
@@ -9,16 +9,39 @@ struct MipsParser;
 
 #[derive(clap::Parser)]
 struct Args {
-    input: String
+    input: String,
+}
+
+impl MipsParser {
+    pub fn encode_imm_instr(args: Pairs<Rule>) -> String {
+        "".into()
+    }
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let input = std::fs::read_to_string(args.input)?;
 
-    let pairs = MipsParser::parse(Rule::program, &input)?;
+    let lines = MipsParser::parse(Rule::program, &input)?;
+    let mut output = String::new();
 
-    println!("{:#?}", pairs);
+    for line in lines {
+        for instruction in line.into_inner() {
+            let op = instruction.as_rule();
+            let args = instruction.into_inner();
+
+            println!("{:#?}", op);
+
+            let hex = match op {
+                Rule::imm_instruction => MipsParser::encode_imm_instr(args),
+                _ => panic!("Error parsing: expected instruction"),
+            };
+
+            output = [output, hex].join("\n");
+        }
+    }
+
+    println!("OUTPUT:\n{}", output);
 
     Ok(())
 }
